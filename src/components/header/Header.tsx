1b88b84e9ec820ea260d084ea9df31218656e7db
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2023-03-10 00:14:27 +0700

    add icon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import style from "./Header.module.scss";
+import { BeakerIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
-  return <div className={style.test}>Header</div>;
+  return (
+    <div className={style.test}>
+      hi
+      <BeakerIcon className={style.icon}></BeakerIcon>
+    </div>
+  );
 };
 
 export default Header;
