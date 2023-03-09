0823b9395e44259c5249f850078a63b2707f64e0
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2023-03-09 22:06:52 +0700

    add router

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Study } from "./study/Study";
+export { default as Document } from "./document/Document";
+export { default as Exercise } from "./exercise/Exercise";
+export { default as Profile } from "./profile/Profile";
+export { default as Forum } from "./forum/Forum";
