15df425ce15114f7391e3b778ae55c5703e59447
KhanhHuyen511 <111450398+KhanhHuyen511@users.noreply.github.com>
2023-03-10 21:39:42 +0700

    Create footer (#3)
    * create footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import classNames from "classnames/bind";
+import React from "react";
+import style from "./Footer.module.scss";
+const cx = classNames.bind(style);
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={cx("footer")}>
+      Terms & Conditions
+      <embed src="images/facebook-icon.svg"></embed>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
