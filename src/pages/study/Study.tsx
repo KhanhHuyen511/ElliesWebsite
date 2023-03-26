2e844e87390a32c5e58bf85ab246d4768a9c6c7d
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2023-03-26 08:37:20 +0700

    study page ui

--- src/pages/study/Study.tsx
+++ src/pages/study/Study.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import styles from './Study.module.scss';
 import {
   Button,
   Input,
@@ -6,10 +7,37 @@ import {
   CheckinPanel,
   CategoryPanel,
   AnswerPanel,
-} from "../../components";
+} from '../../components';
+import classNames from 'classnames/bind';
+import { Col, Row } from 'react-flexbox-grid';
+const cx = classNames.bind(styles);
 
 const Study = () => {
-  return <div></div>;
+  const days = [1, 2, 3, 4, 5, 6, 7];
+  const routes = [8, 9, 10, 11];
+
+  const generateCheckInList = days.map((item, index) => (
+    <li className={cx('check-in-item')}>
+      <CheckinPanel label={'day' + item} />
+    </li>
+  ));
+
+  const generateRouteList = routes.map((item, index) => (
+    <li className={cx('route-study-item')}>
+      <StudyRoute label={'Chặng' + item} />
+    </li>
+  ));
+
+  return (
+    <div className='container'>
+      <Col xs={12} md={8} lg={6}>
+        <p className={cx('title')}>Chào Huyền,</p>
+        <ul className={cx('check-in-wrapper')}>{generateCheckInList}</ul>
+        <p className={cx('page-title')}>Lộ trình</p>
+        <ul className={cx('route-study-wrapper')}>{generateRouteList}</ul>
+      </Col>
+    </div>
+  );
 };
 
 export default Study;
