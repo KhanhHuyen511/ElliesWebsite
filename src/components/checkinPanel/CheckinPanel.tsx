7501872ca92de0fb83185b4d3972e21b339ca1b3
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2023-03-26 10:29:15 +0700

    set up redux

--- src/components/checkinPanel/CheckinPanel.tsx
+++ src/components/checkinPanel/CheckinPanel.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import classNames from "classnames/bind";
-import style from "./CheckinPanel.module.scss";
-import { FireIcon } from "@heroicons/react/24/outline";
+import React from 'react';
+import classNames from 'classnames/bind';
+import style from './CheckinPanel.module.scss';
+import { FireIcon } from '@heroicons/react/24/outline';
 const cx = classNames.bind(style);
 
 interface Props {
-  isActive?: boolean;
+  state?: 'current' | 'before' | 'default' | '';
+  isChecked?: boolean;
   label?: string;
 }
 
 const CheckinPanel = (props: Props) => {
   return (
-    <div className={cx("wrapper", { active: props.isActive })}>
-      <FireIcon className={cx("icon")} />
-      <p className={cx("label")}>{props.label}</p>
+    <div className={cx('wrapper', props.state, { checked: props.isChecked })}>
+      <FireIcon className={cx('icon')} />
+      <p className={cx('label')}>{props.label}</p>
     </div>
   );
 };
--- src/pages/study/Study.tsx
+++ src/pages/study/Study.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Study.module.scss';
 import {
   Button,
@@ -13,12 +13,44 @@ import { Col, Row } from 'react-flexbox-grid';
 const cx = classNames.bind(styles);
 
 const Study = () => {
-  const days = [1, 2, 3, 4, 5, 6, 7];
-  const routes = [8, 9, 10, 11];
+  const [checkedDays, setCheckedDays] = useState(-2);
+  const [currentRoute, setCurrentRoute] = useState(-2);
+
+  // get checked days in this week from Firebase
+
+  const days = [1, 2, 3, 4, 5, 6, 7]; // 7 days in a week, order from 1 to 7
+  const routes = [8, 9, 10, 11]; // show 4 routes
+
+  const d = new Date();
+  console.log(d.getDay());
+  console.log(checkedDays);
+
+  const CheckIn = (item: number) => {
+    // check if item is current day
+    if (item === d.getDay() + 6) setCheckedDays(item);
+  };
+
+  const GetStateCheckIn = (day: number) => {
+    return day === d.getDay() + 6
+      ? 'current'
+      : day <= d.getDay() + 5
+      ? 'before'
+      : 'default';
+  };
+
+  const CheckCheckedDays = (day: number) => {
+    return day === checkedDays;
+  };
+
+  const GetCurrentRoute = () => {};
 
   const generateCheckInList = days.map((item, index) => (
-    <li className={cx('check-in-item')}>
-      <CheckinPanel label={'day' + item} />
+    <li className={cx('check-in-item')} onClick={() => CheckIn(item)}>
+      <CheckinPanel
+        label={'day' + item}
+        isChecked={CheckCheckedDays(item)}
+        state={GetStateCheckIn(item)}
+      />
     </li>
   ));
 
