b4cb30374c7ce9e232db8b9549cab8e9ebc46727
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2023-05-13 12:48:25 +0700

    .

--- src/pages/document/Document.tsx
+++ src/pages/document/Document.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Col } from 'react-flexbox-grid';
+import styles from './Document.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
 
 const Document = () => {
   return (
-    <div>Document</div>
-  )
+    <>
+      <div className='container'>
+        <Col xs={12} md={8} lg={6}>
+          <p className={cx('page-title')}>Tài liệu</p>
+          <ul className={cx('')}>{}</ul>
+        </Col>
+      </div>
+    </>
+  );
 }
 
 export default Document
