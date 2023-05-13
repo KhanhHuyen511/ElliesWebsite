293714e149cffbe949167b675fb83d685dfd5c2f
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2023-05-13 15:57:00 +0700

    update ui of document page

--- src/pages/document/Document.tsx
+++ src/pages/document/Document.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { Col } from 'react-flexbox-grid';
 import styles from './Document.module.scss';
 import classNames from 'classnames/bind';
+import { CategoryPanel, DocCard } from '../../components';
+import {
+  AcademicCapIcon,
+  BookOpenIcon,
+  FireIcon,
+  QueueListIcon,
+  SpeakerWaveIcon,
+} from '@heroicons/react/24/outline';
 const cx = classNames.bind(styles);
 
 const Document = () => {
@@ -10,11 +18,37 @@ const Document = () => {
       <div className='container'>
         <Col xs={12} md={8} lg={6}>
           <p className={cx('page-title')}>Tài liệu</p>
-          <ul className={cx('')}>{}</ul>
+          <ul className={cx('doc-cate-wrapper')}>
+            <CategoryPanel
+              label={'Vocabs'}
+              isActive
+              classNames={cx('cate-item')}
+              icon={<AcademicCapIcon />}
+            />
+            <CategoryPanel
+              label={'Sentences'}
+              classNames={cx('cate-item')}
+              icon={<QueueListIcon />}
+            />
+            <CategoryPanel
+              label={'Audios'}
+              classNames={cx('cate-item')}
+              icon={<SpeakerWaveIcon />}
+            />
+            <CategoryPanel
+              label={'Books'}
+              classNames={cx('cate-item')}
+              icon={<BookOpenIcon />}
+            />
+          </ul>
+          <p className={cx('sub-title')}>Nổi bật</p>
+          <ul className={cx('list')}>
+            <DocCard />
+          </ul>
         </Col>
       </div>
     </>
   );
-}
+};
 
-export default Document
+export default Document;
