8a4ecdc7009a78fbe897c26ee603e8923ff9b1e8
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2023-05-28 00:34:40 +0700

    update info section

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../redux/store';
 import style from './Profile.module.scss';
 import classNames from 'classnames/bind';
 import { FireIcon, PencilIcon } from '@heroicons/react/24/outline';
 import { Button, Input, TextArea } from '../../components';
-import { Student } from '../../types';
+import EditProfile from './EditProfile';
 import { getCurrentStudent } from '../../redux/slice/studentSlice';
+import { formatDate } from '../../utils';
 const cx = classNames.bind(style);
 
 const Profile = () => {
@@ -16,125 +17,145 @@ const Profile = () => {
 
   const user = useSelector((state: RootState) => state.student.currentUser);
 
+  const [isOpenEditForm, setISOpenEditForm] = useState<boolean>(false);
+
   useEffect(() => {
     dispatch(getCurrentStudent(userID));
-  });
+  }, [dispatch, userID]);
 
   return (
-    <div className='container'>
-      <p className={cx('user-name')}>{user?.name}</p>
-      <div className={cx('section-1')}>
-        <div className={cx('avatar-wrapper')}>
-          <div className={cx('avatar')}>
-            <img src='/images/avatar.png' className={cx('avatar-img')} alt='' />
+    <>
+      <div className={'container'}>
+        <p className={cx('user-name')}>{user?.name}</p>
+        <div className={cx('section-1')}>
+          <div className={cx('avatar-wrapper')}>
+            <div className={cx('avatar')}>
+              <img
+                src='/images/avatar.png'
+                className={cx('avatar-img')}
+                alt=''
+              />
+            </div>
+          </div>
+
+          <div className={cx('stats')}>
+            <div className={cx('stat-item')}>
+              <FireIcon className={cx('stat-item-icon')} />
+              <p className={cx('stat-item-label')}>20/300</p>
+            </div>
+            <div className={cx('stat-item')}>
+              <FireIcon className={cx('stat-item-icon')} />
+              <p className={cx('stat-item-label')}>20/300</p>
+            </div>
+            <div className={cx('stat-item')}>
+              <FireIcon className={cx('stat-item-icon')} />
+              <p className={cx('stat-item-label')}>20/300</p>
+            </div>
+            <div className={cx('stat-item')}>
+              <FireIcon className={cx('stat-item-icon')} />
+              <p className={cx('stat-item-label')}>20/300</p>
+            </div>
           </div>
         </div>
 
-        <div className={cx('stats')}>
-          <div className={cx('stat-item')}>
-            <FireIcon className={cx('stat-item-icon')} />
-            <p className={cx('stat-item-label')}>20/300</p>
+        <div className={cx('check-in')}>???</div>
+        <div className={cx('info-section', 'section')}>
+          <div className={cx('info-title-wrapper')}>
+            <p className={cx('section-title')}>Thông tin cá nhân</p>
+            <PencilIcon
+              className={cx('edit-icon')}
+              onClick={() => setISOpenEditForm(true)}
+            />
           </div>
-          <div className={cx('stat-item')}>
-            <FireIcon className={cx('stat-item-icon')} />
-            <p className={cx('stat-item-label')}>20/300</p>
-          </div>
-          <div className={cx('stat-item')}>
-            <FireIcon className={cx('stat-item-icon')} />
-            <p className={cx('stat-item-label')}>20/300</p>
-          </div>
-          <div className={cx('stat-item')}>
-            <FireIcon className={cx('stat-item-icon')} />
-            <p className={cx('stat-item-label')}>20/300</p>
-          </div>
-        </div>
-      </div>
 
-      <div className={cx('check-in')}>???</div>
-      <div className={cx('info-section', 'section')}>
-        <div className={cx('info-title-wrapper')}>
-          <p className={cx('section-title')}>Thông tin cá nhân</p>
-          <PencilIcon className={cx('edit-icon')} />
+          <div className={cx('info-body')}>
+            <Input
+              label={'Email'}
+              value={user?.email}
+              placeholder={'abc@gm.uit.edu.vn'}
+              onChange={() => {}}
+              isDisabled
+            />
+            <Input
+              label={'Tên'}
+              value={user?.name}
+              placeholder={''}
+              onChange={() => {}}
+              isDisabled
+            />
+            <Input
+              label={'Giới tính'}
+              value={user?.gender}
+              placeholder={''}
+              onChange={() => {}}
+              isDisabled
+            />
+            <Input
+              label={'Ngày sinh'}
+              type='date'
+              value={user?.birthday ? formatDate(user?.birthday) : undefined}
+              placeholder={''}
+              onChange={(e) => {}}
+              isDisabled
+            />
+            <TextArea
+              label={'Tiểu sử'}
+              value={user?.bio}
+              placeholder={'Nói gì đó về bạn'}
+              onChange={() => {}}
+              isDisabled
+            />
+          </div>
+        </div>
+        <div className={cx('blog-section', 'section')}>
+          <p className={cx('section-title')}>Liên kết mạng xã hội</p>
+          <div className={cx('blog-body')}>
+            <Button
+              isPrimary={false}
+              onClick={() => {}}
+              className={cx('blog-button')}
+            >
+              Bài viết của tôi
+            </Button>
+            <Button
+              isPrimary={false}
+              onClick={() => {}}
+              className={cx('blog-button')}
+            >
+              Câu hỏi của tôi
+            </Button>
+          </div>
+        </div>
+        <div className={cx('social-section', 'section')}>
+          <p className={cx('section-title')}>Liên kết mạng xã hội</p>
+          <div className={cx('social-body')}></div>
+        </div>
+        <div className={cx('account-section', 'section')}>
+          <p className={cx('section-title')}>Tài khoản</p>
+          <div className={cx('account-body')}>
+            <Button
+              isPrimary={false}
+              onClick={() => {}}
+              className={cx('account-button')}
+            >
+              Khóa tài khoản
+            </Button>
+          </div>
         </div>
 
-        <div className={cx('info-body')}>
-          <Input
-            label={'Email'}
-            value={user?.email}
-            placeholder={'abc@gm.uit.edu.vn'}
-            onChange={() => {}}
-            isDisabled
-          />
-          <Input
-            label={'Tên'}
-            value={user?.name}
-            placeholder={''}
-            onChange={() => {}}
-            isDisabled
-          />
-          <Input
-            label={'Giới tính'}
-            value={user?.gender}
-            placeholder={''}
-            onChange={() => {}}
-            isDisabled
-          />
-          <Input
-            label={'Ngày sinh'}
-            value={''}
-            placeholder={''}
-            onChange={() => {}}
-            isDisabled
-          />
-          <TextArea
-            label={'Tiểu sử'}
-            value={user?.bio}
-            placeholder={'Nói gì đó về bạn'}
-            onChange={() => {}}
-            isDisabled
-          />
-        </div>
-      </div>
-      <div className={cx('blog-section', 'section')}>
-        <p className={cx('section-title')}>Liên kết mạng xã hội</p>
-        <div className={cx('blog-body')}>
-          <Button
-            isPrimary={false}
-            onClick={() => {}}
-            className={cx('blog-button')}
-          >
-            Bài viết của tôi
-          </Button>
-          <Button
-            isPrimary={false}
-            onClick={() => {}}
-            className={cx('blog-button')}
-          >
-            Câu hỏi của tôi
-          </Button>
-        </div>
-      </div>
-      <div className={cx('social-section', 'section')}>
-        <p className={cx('section-title')}>Liên kết mạng xã hội</p>
-        <div className={cx('social-body')}></div>
-      </div>
-      <div className={cx('account-section', 'section')}>
-        <p className={cx('section-title')}>Tài khoản</p>
-        <div className={cx('account-body')}>
-          <Button
-            isPrimary={false}
-            onClick={() => {}}
-            className={cx('account-button')}
-          >
-            Khóa tài khoản
-          </Button>
-        </div>
-      </div>
+        <Button isPrimary={false} onClick={() => {}} className={cx('log-out')}>
+          Đăng xuất
+        </Button>
 
-      <Button isPrimary={false} onClick={() => {}} className={cx('log-out')}>
-        Đăng xuất
-      </Button>
-    </div>
+        {isOpenEditForm && user && (
+          <EditProfile
+            data={user}
+            isDisplay={isOpenEditForm}
+            onClose={() => setISOpenEditForm(false)}
+          ></EditProfile>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/redux/slice/studentSlice.ts
+++ src/redux/slice/studentSlice.ts
@@ -6,9 +6,11 @@ import {
   getDoc,
   getDocs,
   query,
+  updateDoc,
   where,
 } from 'firebase/firestore';
 import { db } from '../../firebase/config';
+import { getDate } from '../../utils';
 
 interface types {
   currentUser?: Student;
@@ -16,14 +18,38 @@ interface types {
 
 const initialState: types = {};
 
-// Write reducer get Docs
+// Write reducer get Doc
 export const getCurrentStudent = createAsyncThunk(
   'student/get',
   async (userID: string) => {
     var stu: Student;
     const q = query(collection(db, 'students'), where('id', '==', userID));
-    stu = (await getDocs(q)).docs[0].data() as Student;
+    const querySnapshot = (await getDocs(q)).docs[0];
+    stu = querySnapshot.data() as Student;
+
+    stu.birthday = getDate(querySnapshot.data().birthday.seconds);
+
     return stu;
+  }
+);
+
+export const updateCurrentStudent = createAsyncThunk(
+  'student/update',
+  async ({ data, oldData }: { data: Student; oldData: Student }) => {
+    const q = query(collection(db, 'students'), where('id', '==', data.id));
+    const querySnapshot = (await getDocs(q)).docs[0];
+
+    console.log(data);
+
+    await updateDoc(querySnapshot.ref, {
+      name: data.name ? data.name : oldData.name,
+      email: data.email ? data.email : oldData.email,
+      birthday: data.birthday ? data.birthday : oldData.birthday,
+      gender: data.gender ? data.gender : oldData.gender,
+      bio: data.bio ? data.bio : oldData.bio,
+    });
+
+    return data;
   }
 );
 
