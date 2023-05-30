1efeff9d61b86532dcc79076c7219a6db76c1037
Huyen Nguyen [Ellie] <huyen.nguyen@navercorp.com>
2023-05-30 16:05:00 +0700

    update ui create blog page

--- src/pages/forum/CreateBlog.tsx
+++ src/pages/forum/CreateBlog.tsx
@@ -7,43 +7,45 @@ import { Button, Input } from '../../components';
 const cx = classNames.bind(style);
 
 const CreateBlog = () => {
-  const [content, setContent] = useState('');
   return (
     <>
       <div className='container'>
-        <p className={cx('title')}>Tạo bài viết</p>
+        <p className={cx('page-title')}>Tạo bài viết</p>
         <Input label={'Tiêu đề'} placeholder={''} onChange={() => {}} />
         <Input label={'Từ khoá'} placeholder={''} onChange={() => {}} />
-        <ReactQuill
-          theme='snow'
-          value={''}
-          placeholder={'Nhập nội dung'}
-          onChange={(content, delta, source, editor) => {}}
-          modules={{
-            toolbar: {
-              container: [
-                [{ header: [1, 2, 3, 4, 5, 6, false] }],
-                ['bold', 'italic', 'underline'],
-                [{ list: 'ordered' }, { list: 'bullet' }],
-                [{ align: [] }],
-                ['link', 'image'],
-                ['clean'],
-                [{ color: [] }],
-              ],
-              handlers: {
-                // image: this.imageHandler
+        <div className={cx('text-editor')}>
+          <p className={cx('text-editor-label')}>Nội dung</p>
+          <ReactQuill
+            theme='snow'
+            value={''}
+            placeholder={'Nhập nội dung'}
+            onChange={(content, delta, source, editor) => {}}
+            modules={{
+              toolbar: {
+                container: [
+                  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                  ['bold', 'italic', 'underline'],
+                  [{ list: 'ordered' }, { list: 'bullet' }],
+                  [{ align: [] }],
+                  ['link', 'image'],
+                  ['clean'],
+                  [{ color: [] }],
+                ],
+                handlers: {
+                  // image: this.imageHandler
+                },
               },
-            },
-            // table: true,
-          }}
-        />
-        <div
+              // table: true,
+            }}
+          />
+        </div>
+        {/* <div
           dangerouslySetInnerHTML={{
             __html: 'hi',
           }}
-        ></div>
+        ></div> */}
         <div className={cx('cta')}>
-          <Button isPrimary={true} onClick={() => {}}>
+          <Button isPrimary={true} onClick={() => {}} className={cx('submit')}>
             Đăng
           </Button>
         </div>
