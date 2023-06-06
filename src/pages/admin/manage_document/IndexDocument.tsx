78c95014763792896c6307c690a912505a263ae5
Huyen Nguyen [Ellie] <huyen.nguyen@navercorp.com>
2023-06-06 23:39:26 +0700

    [admin] create tab in doc page

--- src/pages/admin/manage_document/IndexDocument.tsx
+++ src/pages/admin/manage_document/IndexDocument.tsx
@@ -5,12 +5,23 @@ import VocabDoc from './VocabDoc';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../../redux/store';
 import { getVocabs } from '../../../redux/slice/adminSlice';
+import { CategoryPanel } from '../../../components';
+import {
+  AcademicCapIcon,
+  BookOpenIcon,
+  QueueListIcon,
+  SpeakerWaveIcon,
+} from '@heroicons/react/24/outline';
 
 const cx = classNames.bind(styles);
 
 const IndexDocument = () => {
   const dispatch = useDispatch<AppDispatch>();
+
   const listVocabs = useSelector((state: RootState) => state.admin.listVocabs);
+  const listSentences = [];
+
+  const [currentType, setCurrentType] = useState('vocabs');
 
   useEffect(() => {
     dispatch(getVocabs());
@@ -23,7 +34,44 @@ const IndexDocument = () => {
         <div className={cx('section')}>
           <h2>Manage Document</h2>
 
-          <VocabDoc list={listVocabs} />
+          <ul className={cx('doc-cate-wrapper')}>
+            <CategoryPanel
+              label={'Vocabs'}
+              isActive={currentType === 'vocabs'}
+              classNames={cx('cate-item')}
+              icon={<AcademicCapIcon />}
+              onClick={() => setCurrentType('vocabs')}
+            />
+            <CategoryPanel
+              label={'Sentences'}
+              isActive={currentType === 'sentences'}
+              classNames={cx('cate-item')}
+              icon={<QueueListIcon />}
+              onClick={() => setCurrentType('sentences')}
+            />
+            <CategoryPanel
+              label={'Audios'}
+              isActive={currentType === 'audios'}
+              classNames={cx('cate-item')}
+              icon={<SpeakerWaveIcon />}
+              onClick={() => setCurrentType('audios')}
+            />
+            <CategoryPanel
+              label={'Books'}
+              isActive={currentType === 'books'}
+              classNames={cx('cate-item')}
+              icon={<BookOpenIcon />}
+              onClick={() => setCurrentType('books')}
+            />
+          </ul>
+
+          {currentType === 'vocabs' ? (
+            <VocabDoc list={listVocabs} />
+          ) : currentType === 'sentences' ? (
+            <></>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </>
