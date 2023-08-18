53291a2b370efb34cc2680b0cdd6ae12a1246c8c
huyen-nguyen <huyen.nguyen@navercorp.com>
2023-08-18 11:29:30 +0700

    search saved items

--- src/pages/saved/SavedPage.tsx
+++ src/pages/saved/SavedPage.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Col } from "react-flexbox-grid";
 import style from "./SavedPage.module.scss";
 import classNames from "classnames/bind";
-import { Input } from "../../components";
+import { Button, Input } from "../../components";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../redux/store";
@@ -19,12 +19,23 @@ const SavedPage = () => {
 
   const dispatch = useDispatch<AppDispatch>();
 
+  let search: string;
+
+  const [searchSubmit, setSearchSubmit] = useState(false);
+  const [searchList, setSearchList] = useState<StudyCard[]>();
+
+  const [filter, setFilter] = useState("All");
+
   useEffect(() => {
     if (userID) dispatch(getAllSaved(userID));
+    console.log("render");
   }, [userID, dispatch]);
 
   const getSavedList = () => {
-    return savedList.map((item, index) => SavedItem(item, index));
+    if (!searchSubmit) {
+      return savedList.map((item, index) => SavedItem(item, index));
+    }
+    return searchList?.map((item, index) => SavedItem(item, index));
   };
 
   const removeFromSavedSection = (item: StudyCard) => {
@@ -62,22 +73,68 @@ const SavedPage = () => {
     );
   };
 
+  const searchSavedByName = (keyword: string) => {
+    setSearchList(
+      savedList.filter((o) =>
+        o.display?.toLowerCase().includes(keyword.toLowerCase())
+      )
+    );
+  };
+
   return (
     <>
       <div className={cx("wrapper", "container")}>
         <Col xs={12} md={8} lg={6}>
           <p className={cx("page-title")}>Saved</p>
 
-          <Input
-            label={"Search"}
-            placeholder={"Enter your keyword"}
-            onChange={() => {}}
-          ></Input>
+          <div className={cx("search-wrapper")}>
+            <Input
+              label={"Search"}
+              placeholder={"Enter your keyword"}
+              onChange={(e) => {
+                search = e.target.value;
+              }}
+              className={cx("input")}
+            ></Input>
+            <Button
+              isPrimary={false}
+              onClick={() => {
+                if (search && search !== " ") {
+                  searchSavedByName(search);
+                  setSearchSubmit(true);
+                }
+              }}
+              className={cx("button")}
+            >
+              Search
+            </Button>
+          </div>
 
           <ul className={cx("search-list")}>
-            <li className={cx("item", "is-active")}>All</li>
-            <li className={cx("item")}>Word</li>
-            <li className={cx("item")}>Sentence</li>
+            <li
+              className={cx("item", { "is-active": filter === "All" })}
+              onClick={() => {
+                setFilter("All");
+              }}
+            >
+              All
+            </li>
+            <li
+              className={cx("item", { "is-active": filter === "Word" })}
+              onClick={() => {
+                setFilter("Word");
+              }}
+            >
+              Word
+            </li>
+            <li
+              className={cx("item", { "is-active": filter === "Sentence" })}
+              onClick={() => {
+                setFilter("Sentence");
+              }}
+            >
+              Sentence
+            </li>
           </ul>
 
           <ul className={cx("saved-list")}>{getSavedList()}</ul>
